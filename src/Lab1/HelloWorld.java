1166461de62193e5ee527b76beef4d39af482ad7
Wachraiay <Wachiraya.s@rmutsvmail.com>
2020-07-15 14:25:12 +0700

    Initial commit

---
+++ src/Lab1/HelloWorld.java
@@ -0,0 +1,19 @@
+package Lab1;
+
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+public class HelloWorld
+{
+    public static void main(String[] args) {
+
+        System.out.println("Hello Word");
+
+
+
+
+
+
+
+    }
+
+}
