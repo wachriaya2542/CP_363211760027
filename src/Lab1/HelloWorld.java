fdcbf464fada80c45ca35cce027fac9da7ab22bd
Wachraiay <wachiraya.s@rmutsvmail.com>
2020-07-15 14:55:41 +0700

    add more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -6,7 +6,13 @@ public class HelloWorld
 {
     public static void main(String[] args) {
 
-        System.out.println("Hello Word");
+        System.out.println("วชิรญาณ์");
+        System.out.println("363211760027");
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ");
+        System.out.println("เทคโนโลยีการจัดการ");
+
+
+
 
 
 
